86d60ec8844b58f7a2a29b7aa4c4c5461e674b5b
Ajila K Johnkutty <kjajila@gmail.com>
2023-03-17 13:23:31 -0400

    set up server.js and robo3T

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,6 @@
 const { connect, connection } = require('mongoose');
 
-connect('mongodb://localhost/wikiDB', {
+connect('mongodb://localhost:27017/wikiDB', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
--- server.js
+++ server.js
@@ -1,14 +1,26 @@
 const express = require('express');
+const bodyParser = require("body-parser");
+const ejs = require('ejs');
 const db = require('./config/connection');
 // const routes = require('./routes');
-
+const mongoose = require('mongoose');
 
 const PORT = process.env.port || 3001;
 const app = express();
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'));
+// app.use(express.json());
 // app.use(routes);
+
+const articleSchema ={
+    title:String,
+    content:String
+};
+
+const Article = mongoose.model('Article', articleSchema)
+
 
 db.once('open', () => {
   app.listen(PORT, () => {
